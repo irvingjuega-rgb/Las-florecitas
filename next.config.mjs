bb6d1decb6e13d48933587e08c0ceb9cb316e372
Irving Yoav Coronado Gutierrez <irvingcg@MacBook-Air-de-Irving-7.local>
2026-03-10 18:02:24 -0600

    Configuracion de core-policy

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        // Se aplica a todas las rutas dentro de /api/
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Reemplazar con dominios específicos en producción si es necesario
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
+  }
 }
 
 export default nextConfig
